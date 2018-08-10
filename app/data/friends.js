f60ac7dc538eec32be90df5bb4afbe6e389d114a
William Budoyo <williambu07@gmail.com>
2018-08-09 20:37:42 -0400

    we finally can post the data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-let friendData=
+let friendData=[
 {
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -15,6 +15,6 @@ let friendData=
         1
       ]
   }
-  
+];  
 
 module.exports =friendData;
