d1c34a0c3cce0f3fca0ee3d4d3cff100c56928a2
William Budoyo <williambu07@gmail.com>
2018-08-10 21:17:01 -0400

    validation and modal worked

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,37 @@
 let friendData=[
 {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+    "name": "SpongedBob",
+    "photo": "https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg",
+    "scores": [ 
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
       ]
-  }
+  },
+  {
+    "name": "adcs",
+    "photo": "https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg",
+    "scores": [ 
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
+      ]
+  },
+  
 ];  
 
 module.exports =friendData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,27 @@ module.exports = function(app) {
       });
 
     app.post("/api/friends", function(req,res){
-        friendsData.push(req.body);
+        //Convert each user's results into a simple array of numbers
+        newUserScores=[];
+
+    // get the score from new user and push them to the array
+    // get the new user data and compare them with each existed user scores
+    // store the value into variables
+    //  when the loop is getting the next existed user data
+    // and compare the value when we first store the array
+    // if it bigger let it go
+    //  if it less change the value
+    // after the looping done
+    //  print the lowest data
+
+        // newUserScores.push(req.body);
+        // console.log(newUserScores);
+        // console.log(newUserScores[0].scores);
+
+		// for(i=0;  i<10; i++){
+        //     newUserScores[0].scores[i] - friendsData[0]
+        // }
+            res.json(friendsData[0]);
+		// friendsData.push(req.body);
     });
 };
